a1a45ccd5aa276d3b377bb2af27e47157b824b8c
Joaquim Medeiros <joaquim@ekipa.de>
2021-05-11 21:39:13 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const getAdvice = (container) =>  {
+  fetch('https://api.adviceslip.com/advice')
+  .then(response => response.json())
+  .then(json => container.innerHTML = `<div>${json.slip.advice}</div>`)
+} 
+  
+
+
+document.addEventListener("DOMContentLoaded", function(){
+  rootContainer = document.getElementById('root');
+  getAdvice(rootContainer)
+  setInterval(() => 
+  getAdvice(rootContainer)
+  , 1000);  
+});
