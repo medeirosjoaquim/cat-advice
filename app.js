d0367b43e547f1126bf01e62c65b7b5339632be8
JohnBox <johnboxcodes@gmail.com>
2021-05-11 22:39:12 -0300

    adds fade

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const getAdvice = (container) =>  {
-  fetch('https://api.adviceslip.com/advice')
+const getAdvice = async (container) =>  {
+  container.classList.add('fade')
+   await fetch('https://api.adviceslip.com/advice')
   .then(response => response.json())
   .then(json => container.innerHTML = `<div>${json.slip.advice}</div>`)
+  container.classList.remove('fade')
 } 
   
 
 
 document.addEventListener("DOMContentLoaded", function(){
+  
   rootContainer = document.getElementById('root');
-  getAdvice(rootContainer)
-  setInterval(() => 
-  getAdvice(rootContainer)
-  , 8000);  
+  setInterval(() =>  getAdvice(rootContainer)
+  , 6000);  
 });
